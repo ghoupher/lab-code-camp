30fdbeeb58f9a033196b39ec5a1081c9f9524763
rodrigmars <rodrigmars@gmail.com>
2024-05-30 19:24:10 -0300

    Base estrutura sendo criada

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("ola mundo")
+require('dotenv').config()
+
+
+console.log("S3_BUCKET::", process.env.S3_BUCKET)
+console.log("SECRET_KEY::", process.env.SECRET_KEY)
+
+console.log("ola mundo do rodrigo")
